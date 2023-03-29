e77ad3c9a21ff62eedad40f6d8d45c1445bae816
ibrahimkoz <ibrahimkoz.cs@gmail.com>
2023-03-29 22:22:09 +0300

    Added TS support

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,55 @@
+import type {Writable} from "svelte/store";
 import {writable} from "svelte/store";
 
+type Subscribable<T> = Omit<Writable<T>, "update" | "set">;
+type ReactiveInstance<T> = T & Subscribable<T>
 
-export function createReactiveInstanceOf(
-    constructor,
-    ...args
-) {
+const commands = Symbol('commands')
+
+type Command = (...args: unknown[]) => void;
+
+const emptySet = new Set();
+
+export function createReactiveInstanceOf<T, S extends unknown[]>(
+    constructor: new (...args: S) => T,
+    ...args: S
+): ReactiveInstance<T> {
     const instance = new constructor(...args);
-    const store = writable(instance)
+    const store = writable(instance);
 
+    const commandsNames: Set<string> = constructor.prototype[commands] ?? emptySet;
 
-    const commandsNames = constructor.prototype['commands'] ?? []
+    const reactiveCommandOf = (command: Command) => {
+        return (...args: unknown[]) => {
+            command(...args);
+            store.set({...instance});
+        };
+    };
 
-    const set = store.set
+    const commandPairs: [string, Command][] = [...commandsNames].map(it => [
+        it,
+        constructor.prototype[it].bind(instance),
+    ]);
 
-    const _set = () => {
-        set.bind(store, {...instance})()
+    const reactiveCommands = commandPairs.reduce((acc, [name, command]) => {
+        return {...acc, [name]: reactiveCommandOf(command)};
+    }, store);
+
+    return {
+        ...instance,
+        ...reactiveCommands,
+        subscribe: store.subscribe,
     }
-
-    for (let methodName of commandsNames) {
-        const method = constructor.prototype[methodName]
-        const methodBound = method.bind(instance)
-        store[methodName] = (...args) => {
-            methodBound(...args)
-            _set();
-        }
-    }
-
-    delete store.set
-    delete store.update
-
-    return store
 }
 
 
-export function command(target, name, descriptor) {
-    if (target.commands == undefined) {
-        target.commands = new Set()
+type Target<T> = T & { [commands]?: Set<string> };
+
+export function command<T, S>(target: Target<T>, name: string, descriptor: S): S {
+    if (target[commands] == undefined) {
+        target[commands] = new Set<string>();
     }
 
-    target.commands.add(name)
-    return descriptor
+    target[commands].add(name);
+    return descriptor;
 }
-
